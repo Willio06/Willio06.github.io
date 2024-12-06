677d9570852feb074d367bf926218c63efd1142f
Willio06 <tuur.willio@gmail.com>
2024-12-06 14:25:42 +0100

    update main

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-//toggle var
-var fade_state = true;
-//on btn click
-function fade() {
-  //get the button and div
-  let div = document.getElementById("frontpage");
-  let div2 = document.getElementById("page2")
-  let btn = document.getElementById("buttonfront");
-  //if faded in or out
-  if (fade_state == true) {
-    //trigers animation
-    div.style.animation = "fade-out 0.3s forwards";
-    div.hidden = true
-    div2.hidden=false
-    div2.style.animation = "fade-in 0.5s forwards";
-    
-    //sets the text
-    btn.innerHTML = "fade-in";
-    //sets fade_state
-    fade_state = false;
-  } else if (fade_state == false) {
-    //trigers animation
-    div.hidden=false
-    div.style.animation = "fade-in 0.5s forwards";
-    div2.style.animation = "fade-out 0.3s forwards";
-    div2.hidden = true
+function fade(id) {
+  const sections = ['about', 'education', 'experience', 'skills'];
+  const currentVisibleDiv = sections.find((el) => !document.getElementById(el).hidden); 
+  document.getElementById(currentVisibleDiv).animation= "fade-out 0.3s forwards";
+  document.getElementById(currentVisibleDiv).hidden=true;
+  document.getElementById(id).hidden=false
+  document.getElementById(id).style.animation = "fade-in 0.5s forwards";
 
-    
-    //sets the text
-    btn.innerHTML = "fade-out";
-    //sets fade_state
-    fade_state = true;
-  }
 }
